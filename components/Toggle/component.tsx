794a4fb4551231856190767d264c00ea25637b68
Sergey Lysenko <yamaha001001@gmail.com>
2019-02-20 21:52:00 +0200

    trigger

---
+++ components/Toggle/component.tsx
@@ -0,0 +1,34 @@
+// import * as React from 'react'
+
+// interface IProps {
+//   initSwitchPosition?: boolean,
+//   initText?: string,
+//   hook?: () => void
+// }
+
+// const ToggleComponent = ({ ...props }: IProps) => render(stateLayer(props))
+
+// const stateLayer = ({ initSwitchPosition = false, initText = '', hook = () => null }: IProps) => {
+//   const [ switchPosition , setSwitch ] = React.useState(initSwitchPosition)
+//   const [ text, setText ] = React.useState(initText)
+
+//   const setRevertSwitch = ({ switchPosition, setSwitch }) => (
+//     setSwitch(!switchPosition)
+//   )
+
+//   const changePosition = ({ switchPosition, setSwitch, setRevertSwitch, setText, hook }): void => (
+//     switchPosition
+//       ? [
+//         setRevertSwitch,
+        
+//       ].forEach()
+//   )
+
+//   return ({
+
+//   })
+// }
+
+// const render = () => (
+//   <div>11231</div>
+// )

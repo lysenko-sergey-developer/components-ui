b8b4baa6621996902fd94c94fd5cdf49986695c2
Sergey Lysenko <yamaha001001@gmail.com>
2019-02-22 01:33:58 +0200

    update ci script && remove bug with ci broken on gh-pages

--- README.md
+++ README.md
@@ -18,5 +18,5 @@
 - [ ] Add caching for deploy gh version
 - [x] Add ci deploy trigger on master branch changes
 - [ ] Optimize CI
-- [ ] React hooks examples
-- [ ]
+- [ ] React hooks example
+- [x] Rewrite CI deploy script
--- components/Toggle/component.tsx
+++ components/Toggle/component.tsx
@@ -1,34 +1,34 @@
-import * as React from 'react'
+// import * as React from 'react'
 
-interface IProps {
-  initSwitchPosition?: boolean,
-  initText?: string,
-  hook?: () => void
-}
+// interface IProps {
+//   initSwitchPosition?: boolean,
+//   initText?: string,
+//   hook?: () => void
+// }
 
-const ToggleComponent = ({ ...props }: IProps) => render(stateLayer(props))
+// const ToggleComponent = ({ ...props }: IProps) => render(stateLayer(props))
 
-const stateLayer = ({ initSwitchPosition = false, initText = '', hook = () => null }: IProps) => {
-  const [ switchPosition , setSwitch ] = React.useState(initSwitchPosition)
-  const [ text, setText ] = React.useState(initText)
+// const stateLayer = ({ initSwitchPosition = false, initText = '', hook = () => null }: IProps) => {
+//   const [ switchPosition , setSwitch ] = React.useState(initSwitchPosition)
+//   const [ text, setText ] = React.useState(initText)
 
-  const setRevertSwitch = ({ switchPosition, setSwitch }) => (
-    setSwitch(!switchPosition)
-  )
+//   const setRevertSwitch = ({ switchPosition, setSwitch }) => (
+//     setSwitch(!switchPosition)
+//   )
 
-  const changePosition = ({ switchPosition, setSwitch, setRevertSwitch, setText, hook }): void => (
-    switchPosition
-      ? [
-        setRevertSwitch,
+//   const changePosition = ({ switchPosition, setSwitch, setRevertSwitch, setText, hook }): void => (
+//     switchPosition
+//       ? [
+//         setRevertSwitch,
         
-      ].forEach()
-  )
+//       ].forEach()
+//   )
 
-  return ({
+//   return ({
 
-  })
-}
+//   })
+// }
 
-const render = () => (
-  <div>11231</div>
-)
+// const render = () => (
+//   <div>11231</div>
+// )
